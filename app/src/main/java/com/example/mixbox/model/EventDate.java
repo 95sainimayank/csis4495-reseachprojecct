796c25f48659aecf0e62f035c93bb26e09938d41
95sainimayank <95sainimayank@gmail.com>
2022-06-05 19:12:15 -0700

    Added models and retrofit service for ticketmaster api

---
+++ app/src/main/java/com/example/mixbox/model/EventDate.java
@@ -0,0 +1,17 @@
+package com.example.mixbox.model;
+
+public class EventDate {
+   public Start start;
+
+   public EventDate(Start start) {
+      this.start = start;
+   }
+
+   public Start getStart() {
+      return start;
+   }
+
+   public void setStart(Start start) {
+      this.start = start;
+   }
+}
---
+++ app/src/main/java/com/example/mixbox/model/Venue.java
@@ -0,0 +1,37 @@
+package com.example.mixbox.model;
+
+public class Venue {
+   private String name;
+   private String id;
+   private Location location;
+
+   public Venue(String name, String id,Location location) {
+      this.name = name;
+      this.id = id;
+      this.location = location;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getId() {
+      return id;
+   }
+
+   public void setId(String id) {
+      this.id = id;
+   }
+
+   public Location getLocation() {
+      return location;
+   }
+
+   public void setLocation(Location location) {
+      this.location = location;
+   }
+}
