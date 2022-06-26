d76389be8a12cbe9af1f16cc87abce3a8f728bf0
95sainimayank <95sainimayank@gmail.com>
2022-06-25 22:12:25 -0700

    Fixed sort for maincards

--- app/src/main/java/com/example/mixbox/fragments/SongListFragment.java
+++ app/src/main/java/com/example/mixbox/fragments/SongListFragment.java
@@ -16,6 +16,8 @@ import com.example.mixbox.adapter.SongListAdapter;
 import com.example.mixbox.databinding.FragmentSongListBinding;
 import com.example.mixbox.model.Song;
 import com.example.mixbox.model.SongListModel;
+import com.example.mixbox.utilities.SortSongByLatest;
+import com.example.mixbox.utilities.SortSongByPlayCount;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
@@ -28,6 +30,8 @@ import java.util.HashMap;
 public class SongListFragment extends Fragment {
    FragmentSongListBinding binding;
    FirebaseDatabase db;
+   ArrayList<SongListModel> allSongListItems;
+   SongListAdapter songListAdapter;
 
    public SongListFragment() {
    }
@@ -45,13 +49,12 @@ public class SongListFragment extends Fragment {
 
       binding.songlistRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-      ArrayList<SongListModel> allSongListItems = new ArrayList<>();
+      allSongListItems = new ArrayList<>();
 
-      SongListAdapter songListAdapter = new SongListAdapter(getActivity(), allSongListItems);
+      songListAdapter = new SongListAdapter(getActivity(), allSongListItems);
       binding.songlistRecyclerView.setAdapter(songListAdapter);
 
       String type = getArguments().get("type").toString();
-
 
       binding.toolbar.setNavigationOnClickListener(new View.OnClickListener() {
          @Override
@@ -63,173 +66,23 @@ public class SongListFragment extends Fragment {
 
       switch (type) {
          case "rock":
-            db.getReference().get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-               @Override
-               public void onComplete(@NonNull Task<DataSnapshot> task) {
-                  if (task.isSuccessful()) {
-                     DataSnapshot snapshot = task.getResult();
-                     HashMap<String, Object> outerMap = (HashMap<String, Object>) snapshot.getValue();
-                     HashMap<String, Object> allUsers = (HashMap<String, Object>) outerMap.get("allUsers");
-
-                     for (Object value : allUsers.values()) {
-                        HashMap<String, Object> eachUser = (HashMap<String, Object>) value;
-
-
-                        HashMap<String, Object> eachUserSongs = (HashMap<String, Object>) eachUser.get("songs");
-
-                        if (eachUserSongs != null) {
-                           for (Object song : eachUserSongs.values()) {
-                              HashMap<String, Object> eachSong = (HashMap<String, Object>) song;
-
-
-                              HashMap<String, Object> genreMap = (HashMap<String, Object>) eachSong.get("genre");
-
-                              for (Object genre : genreMap.values()) {
-                                 if (genre.toString().equals("rock")) {
-                                    SongListModel recyclerViewModelObject = new SongListModel();
-
-                                    String name = eachUser.get("fullName").toString();
-                                    recyclerViewModelObject.setArtistName(name);
-
-                                    recyclerViewModelObject.
-                                      setSong(new Song(eachSong.get("songName").toString(),
-                                        Integer.parseInt(eachSong.get("timesPlayed").toString()),
-                                        LocalDateTime.parse(eachSong.get("dateTime").toString()),
-                                        null));
-
-                                    Log.e("---", recyclerViewModelObject.getSong().getSongName());
-
-                                    allSongListItems.add(recyclerViewModelObject);
-                                 }
-                              }
-                           }
-                        }
-
-                     }
-
-
-                     songListAdapter.notifyDataSetChanged();
-                  } else {
-                     Log.e("---", task.getException().toString());
-                  }
-               }
-            });
+            showSongBasedOnCategory("rock");
             break;
          case "edm":
-            db.getReference().get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-               @Override
-               public void onComplete(@NonNull Task<DataSnapshot> task) {
-                  if (task.isSuccessful()) {
-                     DataSnapshot snapshot = task.getResult();
-                     HashMap<String, Object> outerMap = (HashMap<String, Object>) snapshot.getValue();
-                     HashMap<String, Object> allUsers = (HashMap<String, Object>) outerMap.get("allUsers");
-
-                     for (Object value : allUsers.values()) {
-                        HashMap<String, Object> eachUser = (HashMap<String, Object>) value;
-
-
-                        HashMap<String, Object> eachUserSongs = (HashMap<String, Object>) eachUser.get("songs");
-
-                        if (eachUserSongs != null) {
-                           for (Object song : eachUserSongs.values()) {
-                              HashMap<String, Object> eachSong = (HashMap<String, Object>) song;
-
-
-                              HashMap<String, Object> genreMap = (HashMap<String, Object>) eachSong.get("genre");
-
-                              for (Object genre : genreMap.values()) {
-                                 if (genre.toString().equals("edm")) {
-                                    SongListModel recyclerViewModelObject = new SongListModel();
-
-                                    String name = eachUser.get("fullName").toString();
-                                    recyclerViewModelObject.setArtistName(name);
-
-                                    recyclerViewModelObject.
-                                      setSong(new Song(eachSong.get("songName").toString(),
-                                        Integer.parseInt(eachSong.get("timesPlayed").toString()),
-                                        LocalDateTime.parse(eachSong.get("dateTime").toString()),
-                                        null));
-
-                                    Log.e("---", recyclerViewModelObject.getSong().getSongName());
-
-                                    allSongListItems.add(recyclerViewModelObject);
-                                 }
-                              }
-                           }
-                        }
-
-                     }
-
-
-                     songListAdapter.notifyDataSetChanged();
-                  } else {
-                     Log.e("---", task.getException().toString());
-                  }
-               }
-            });
+            showSongBasedOnCategory("edm");
             break;
          case "rnb":
-            db.getReference().get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-               @Override
-               public void onComplete(@NonNull Task<DataSnapshot> task) {
-                  if (task.isSuccessful()) {
-                     DataSnapshot snapshot = task.getResult();
-                     HashMap<String, Object> outerMap = (HashMap<String, Object>) snapshot.getValue();
-                     HashMap<String, Object> allUsers = (HashMap<String, Object>) outerMap.get("allUsers");
-
-                     for (Object value : allUsers.values()) {
-                        HashMap<String, Object> eachUser = (HashMap<String, Object>) value;
-
-
-                        HashMap<String, Object> eachUserSongs = (HashMap<String, Object>) eachUser.get("songs");
-
-                        if (eachUserSongs != null) {
-                           for (Object song : eachUserSongs.values()) {
-                              HashMap<String, Object> eachSong = (HashMap<String, Object>) song;
-
-
-                              HashMap<String, Object> genreMap = (HashMap<String, Object>) eachSong.get("genre");
-
-                              for (Object genre : genreMap.values()) {
-                                 if (genre.toString().equals("rnb")) {
-                                    SongListModel recyclerViewModelObject = new SongListModel();
-
-                                    String name = eachUser.get("fullName").toString();
-                                    recyclerViewModelObject.setArtistName(name);
-
-                                    recyclerViewModelObject.
-                                      setSong(new Song(eachSong.get("songName").toString(),
-                                        Integer.parseInt(eachSong.get("timesPlayed").toString()),
-                                        LocalDateTime.parse(eachSong.get("dateTime").toString()),
-                                        null));
-
-                                    Log.e("---", recyclerViewModelObject.getSong().getSongName());
-
-                                    allSongListItems.add(recyclerViewModelObject);
-                                 }
-                              }
-                           }
-                        }
-
-                     }
-
-
-                     songListAdapter.notifyDataSetChanged();
-                  } else {
-                     Log.e("---", task.getException().toString());
-                  }
-               }
-            });
+            showSongBasedOnCategory("rnb");
             break;
          case "latest":
+            showSongBasedOnMainCards("latest");
             break;
          case "mostPlayed":
+            showSongBasedOnMainCards("mostPlayed");
             break;
          default:
             break;
       }
-
-
       return binding.getRoot();
    }
 
@@ -245,5 +98,113 @@ public class SongListFragment extends Fragment {
       ((AppCompatActivity) getActivity()).getSupportActionBar().show();
    }
 
+   public void showSongBasedOnCategory(String category) {
+      db.getReference().get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+         @Override
+         public void onComplete(@NonNull Task<DataSnapshot> task) {
+            if (task.isSuccessful()) {
+               DataSnapshot snapshot = task.getResult();
+               HashMap<String, Object> outerMap = (HashMap<String, Object>) snapshot.getValue();
+               HashMap<String, Object> allUsers = (HashMap<String, Object>) outerMap.get("allUsers");
+
+               for (Object value : allUsers.values()) {
+                  HashMap<String, Object> eachUser = (HashMap<String, Object>) value;
+
+
+                  HashMap<String, Object> eachUserSongs = (HashMap<String, Object>) eachUser.get("songs");
+
+                  if (eachUserSongs != null) {
+                     for (Object song : eachUserSongs.values()) {
+                        HashMap<String, Object> eachSong = (HashMap<String, Object>) song;
+
+
+                        HashMap<String, Object> genreMap = (HashMap<String, Object>) eachSong.get("genre");
+
+                        for (Object genre : genreMap.values()) {
+                           if (genre.toString().equals(category)) {
+                              SongListModel recyclerViewModelObject = new SongListModel();
+
+                              String name = eachUser.get("fullName").toString();
+                              recyclerViewModelObject.setArtistName(name);
+
+                              recyclerViewModelObject.
+                                setSong(new Song(eachSong.get("songName").toString(),
+                                  Integer.parseInt(eachSong.get("timesPlayed").toString()),
+                                  LocalDateTime.parse(eachSong.get("dateTime").toString()),
+                                  null));
+
+                              Log.e("---", recyclerViewModelObject.getSong().getSongName());
+
+                              allSongListItems.add(recyclerViewModelObject);
+                           }
+                        }
+                     }
+                  }
+               }
+
+               songListAdapter.notifyDataSetChanged();
+            } else {
+               Log.e("---", task.getException().toString());
+            }
+         }
+      });
+   }
+
+   public void showSongBasedOnMainCards(String cardType){
+      db.getReference().get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+         @Override
+         public void onComplete(@NonNull Task<DataSnapshot> task) {
+            if (task.isSuccessful()) {
+               DataSnapshot snapshot = task.getResult();
+               HashMap<String, Object> outerMap = (HashMap<String, Object>) snapshot.getValue();
+               HashMap<String, Object> allUsers = (HashMap<String, Object>) outerMap.get("allUsers");
+
+               for (Object value : allUsers.values()) {
+                  HashMap<String, Object> eachUser = (HashMap<String, Object>) value;
+
+
+                  HashMap<String, Object> eachUserSongs = (HashMap<String, Object>) eachUser.get("songs");
+
+                  if (eachUserSongs != null) {
+                     for (Object song : eachUserSongs.values()) {
+                        HashMap<String, Object> eachSong = (HashMap<String, Object>) song;
+
+
+                        HashMap<String, Object> genreMap = (HashMap<String, Object>) eachSong.get("genre");
+
+
+                        SongListModel recyclerViewModelObject = new SongListModel();
+
+                        String name = eachUser.get("fullName").toString();
+                        recyclerViewModelObject.setArtistName(name);
+
+                        recyclerViewModelObject.
+                          setSong(new Song(eachSong.get("songName").toString(),
+                            Integer.parseInt(eachSong.get("timesPlayed").toString()),
+                            LocalDateTime.parse(eachSong.get("dateTime").toString()),
+                            null));
+
+                        Log.e("---", recyclerViewModelObject.getSong().getSongName());
+
+
+                        allSongListItems.add(recyclerViewModelObject);
+
+                     }
+                  }
+               }
+               if(cardType.equals("latest")){
+                  allSongListItems.sort(new SortSongByLatest());
+               }
+               else {
+                  allSongListItems.sort(new SortSongByPlayCount());
+               }
+
+               songListAdapter.notifyDataSetChanged();
+            } else {
+               Log.e("---", task.getException().toString());
+            }
+         }
+      });
+   }
 
 }
