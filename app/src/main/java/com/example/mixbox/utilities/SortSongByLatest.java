e4979133efafb051c2375a46b7385df601573035
95sainimayank <95sainimayank@gmail.com>
2022-08-06 16:44:43 -0700

    fixed latest song algo

--- app/src/main/java/com/example/mixbox/utilities/SortSongByLatest.java
+++ app/src/main/java/com/example/mixbox/utilities/SortSongByLatest.java
@@ -1,5 +1,7 @@
 package com.example.mixbox.utilities;
 
+import android.util.Log;
+
 import com.example.mixbox.model.SongListModel;
 
 import java.util.Comparator;
@@ -7,11 +9,16 @@ import java.util.Comparator;
 public class SortSongByLatest implements Comparator<SongListModel> {
    @Override
    public int compare(SongListModel obj1, SongListModel obj2) {
-      if(obj1.getSong().getDateTime().isAfter(obj2.getSong().getDateTime())){
+//      Log.e("", obj1.getSong().getSongName() +" obj1 - " + obj1.getSong().getDateTime().toString() +
+    //     obj2.getSong().getSongName() + " obj2 - " + obj2.getSong().getDateTime().toString());
+      if((obj1.getSong().getDateTime()).isAfter(obj2.getSong().getDateTime())){
          return -1;
       }
-      else{
+      else if((obj1.getSong().getDateTime()).isBefore(obj2.getSong().getDateTime())){
          return 1;
+      }
+      else{
+         return 0;
       }
    }
 }
